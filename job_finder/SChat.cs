94ce017888266d7dbd174b639aa9da671ddbaa75
zamoshchanskyi <zamoshchanskyimaksim@gmail.com>
2023-06-12 12:16:11 +0300

    pratcya

---
+++ job_finder/SChat.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace job_finder
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class SChat : ISChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser() {
+                ID = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+
+            SendMsg( " " + user.Name + " в чаті!", 0);
+            users.Add(user);
+
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMsg(" " + user.Name + " вийшов з чату!", 0);
+            }
+        }
+
+        public void SendMsg(string msg, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+
+                var user = users.FirstOrDefault(i => i.ID == id);
+                if (user != null)
+                {
+                    answer += " " + user.Name + ":   ";
+                }
+                answer += msg;
+
+
+
+                item.operationContext.GetCallbackChannel<ISChatCallBack>().MsgCallBack(answer);
+            }
+        }
+    }
+}
---
+++ job_finder/ServerUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace job_finder
+{
+    class ServerUser
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public OperationContext operationContext { get; set; }
+    }
+}
